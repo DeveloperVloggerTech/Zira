7629e1d3fcfcb51b32e1da8029b484b304724fe8
Jimmy <devnilghosh2002@gmail.com>
2018-07-20 06:01:14 +0530

    Update Zira.js

--- Zira.js
+++ Zira.js
@@ -47,7 +47,7 @@ class Zira {
     this.bot.on('ready', () => {
       this.Logger.Success(this.bot.user.username, 'Cluster Ready', `Cluster ${this.id}`);
       this.bot.editStatus({
-        name: `${process.env.PREFIX}help | zira.pw`,
+        name: `${process.env.PREFIX}help`,
         type: 0,
       });
       setInterval(this.utils.postStats, 5000, this);
